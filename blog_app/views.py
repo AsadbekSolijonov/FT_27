4af41a5945bcc241971c1d6e5e43753c4109ffb4
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-02-16 14:00:12 +0500

    16-fev 14:00

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from blog_app.models import Blog  # new
 import random
 import datetime
 
 
 # Create your views here.
 def index(request):
-    context = {
-        "name": "Asadbek",
-        "age": random.randint(1, 24),
-        "time": datetime.datetime.now().strftime('%Y.%m.%d'),
-        "a": random.randint(1, 1000),
-        "b": random.randint(-100, 1000)
-    }
-    c = context['a'] + context['b']
-    c1 = context['a'] * context['b']
+    blogs = Blog.objects.all().order_by('-created')  # new
 
-    return HttpResponse(f"""<h1 style="color: red;">{context['name']} ning yoshi {context['age']} da.</h1>
-    <p>Bugun:<code> {context['time']}</code></p>
-    <p>{context['a']} + {context['b']} = {c}</p>
-    <p style="color: green; border: 1px solid black; width: 200px; padding: 10px; border-radius: 10px;">{context['a']} x {context['b']} = {c1}</p>
-    """)
+    context = {
+        "blogs": blogs
+    }
+    return render(request, 'blog/index.html', context=context)
--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'blog_app/templates'],  # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'blog_app/static/'
+STATIC_FILES_DIRS = (BASE_DIR / 'blog_app/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
